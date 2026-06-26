31baf3c6dcb2b98e4c35d8bf47aac0dab2aa861b
RedHawk70 <aznil2000@outlook.com>
2026-06-26 18:55:57 +0800

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -437,8 +437,8 @@ echo "   - Xray Vless Xhttp None Tls : 80" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "    [OTHER PORTS FOR ALL XRAY ]"  | tee -a log-install.txt
 echo "    ---------------------" | tee -a log-install.txt
-echo "   - Xray Tls                  : 2083,2087,8443,2096"  | tee -a log-install.txt
-echo "   - Xray None Tls             : 2052,8080,2082,2095"  | tee -a log-install.txt
+echo "   - Xray Tls                  : 8443,2083,2087,2096"  | tee -a log-install.txt
+echo "   - Xray None Tls             : 8080,2052,2082,2095"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "    [INFORMASI NOOBZVPN ]"  | tee -a log-install.txt
 echo "    ---------------------" | tee -a log-install.txt
